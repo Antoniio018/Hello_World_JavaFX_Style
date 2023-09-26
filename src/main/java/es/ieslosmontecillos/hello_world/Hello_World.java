2f5e21b0cdbb32eea5076ff2213be49de904deea
Antonio <agomrod215@g.educaand.es>
2023-09-26 11:25:19 +0200

    Hello World, JavaFX Style

--- src/main/java/es/ieslosmontecillos/hello_world/Hello_World.java
+++ src/main/java/es/ieslosmontecillos/hello_world/Hello_World.java
@@ -3,15 +3,12 @@ package es.ieslosmontecillos.hello_world;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
-public class HelloApplication extends Application {
+public class Hello_World extends Application {
     @Override
     public void start(Stage primaryStage) {
         Button btn = new Button();
